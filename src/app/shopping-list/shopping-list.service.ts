28d900d657bac7dbaa3cb262ef46ef4f92acdc86
Ilya Tatsianenka <Ilya_Tatsianenka@epam.com>
2019-07-30 17:24:31 +0300

    #512421355: refactors shopping-list.service.ts.

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -16,16 +16,19 @@ export class ShoppingListService {
 
   addIngredient(ingredient: Ingredient) {
     this.ingredients.push(ingredient);
-    this.ingredientsChanged.emit(this.ingredients.slice());
+    this.ingredientChangedEmit();
   }
 
   addIngredients(ingredients: Ingredient[]) {
     this.ingredients.push(...ingredients);
-    this.ingredientsChanged.emit(this.ingredients.slice());
+    this.ingredientChangedEmit();
   }
 
   deleteIngredientByName(name: string) {
-    this.ingredients = this.ingredients.filter(ingredient => ingredient.name !== name);
+    this.ingredientChangedEmit();
+  }
+
+  ingredientChangedEmit() {
     this.ingredientsChanged.emit(this.ingredients.slice());
   }
 }

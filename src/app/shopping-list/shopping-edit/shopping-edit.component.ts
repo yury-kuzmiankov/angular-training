d4d837f2f4d2fd1a79633a659dce773582fd89db
Ilya Tatsianenka <Ilya_Tatsianenka@epam.com>
2019-08-07 10:13:43 +0300

    #512421376: refactors shopping-edit.component. Renamed the variable.

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -4,15 +4,13 @@ import { ShoppingListService } from '../shopping-list.service';
 import { Ingredient } from 'src/app/shared/model/ingredient.model';
 import { NgForm } from '@angular/forms';
 
-
-
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
   styleUrls: ['./shopping-edit.component.sass']
 })
 export class ShoppingEditComponent implements OnInit, OnDestroy {
-  @ViewChild('f', null) shoppingListForm: NgForm;
+  @ViewChild('shoppingEditForm', null) shoppingListForm: NgForm;
   startedEditingSubscription: Subscription;
   editMode = false;
   editedItemId: number;
@@ -54,8 +52,6 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
 
   onClear() {
     this.editMode = false;
-    console.log(this.shoppingListForm.value);
-    console.log(this.shoppingListForm.form.get('amount').value);
     this.shoppingListForm.reset();
   }
 
